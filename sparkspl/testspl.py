0c6ce2ef9dc30ba54b36bf059ce838bec15e9e44
SoNicKKK <nikolay.ryabykh@gmail.com>
2019-02-27 12:32:53 +0300

    + 'stats' command with all functions except 'mode' + public methods for each spl command + PEP8 compliance.

--- sparkspl/testspl.py
+++ sparkspl/testspl.py
@@ -1,10 +1,12 @@
-from sparkspl import SparkSpl
+''' Example for using SparkSpl class for translating SPL command to pySpark functions '''
+
 import sys
+from sparkspl import SparkSpl
 
-path = sys.argv[1]
-spl = sys.argv[2]
-out_path = sys.argv[3]
+PATH = sys.argv[1]
+SPL = sys.argv[2]
+OUT_PATH = sys.argv[3]
 
-sparkspl = SparkSpl(path)
-sparkspl.spl(spl)
-sparkspl.to_csv(out_path)
+sparkspl = SparkSpl(path=PATH)
+sparkspl.spl(SPL)
+sparkspl.to_csv(OUT_PATH)
